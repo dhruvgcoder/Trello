51d8a31cc0929daeec047f8ebf7e3a4052f71d34
Dhruv Gupta <dhruvgcoder@gmail.com>
2026-05-15 09:18:51 +0530

    unique constrain added for username

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,7 @@ import type { InferSchemaType } from "mongoose"
 
 
 const userSchema = new Schema({
-    username : String ,
+    username : {type : String , required : true , unique : true } ,
     password : {type : String , required : true }
 
 })
