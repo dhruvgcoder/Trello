6105420a5a78638402d5bc891922b88618bbc350
Dhruv Gupta <dhruvgcoder@gmail.com>
2026-05-21 02:59:30 +0530

    org routes updated

--- src/routes/organization.route.ts
+++ src/routes/organization.route.ts
@@ -1,124 +1,25 @@
-
 import { Router } from "express";
 
-import type { Request, Response } from "express";
-import { authMiddleware } from "../authMiddleware.js";
-import { organizationModel, userModel } from "../db.js";
-import { organizationService } from "../services/organization.service.js"
-import { adminMiddleware } from "../adminMiddleware.js"
+import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { adminMiddleware } from "../middlewares/adminMiddleware.js"
+
+import {
+    createOrganization,
+    inviteMemberToOrg,
+    getOrganizations,
+    getMembers,
+    removeMember
+} from "../controllers/organization.controller.js"
 
 
 export const organizationRouter = Router();
 
-organizationRouter.post("/create-organization", authMiddleware, async (req: Request, res: Response) => {
-    const userId = req.userId
-    const { title, description } = req.body
+organizationRouter.post("/create", authMiddleware, createOrganization)
 
-try {
-    if (!userId) {
-        res.status(401).json({
-            msg: "Not authorised"
-        })
-        return
-    }
+organizationRouter.post("/:orgId/members", authMiddleware, inviteMemberToOrg)
 
-    const organization = await organizationModel.create({
-        title: title,
-        description: description,
-        admin: userId,
-        members: [userId]
-    })
-    res.json({
-        msg: "Organization Created",
-        orgId: organization._id
-    })
-}catch(err){
-    res.json({
-        msg : "Error creating organization"
-    })
-    return
-}
+organizationRouter.get("/", authMiddleware, getOrganizations)
 
-})
-organizationRouter.post("/invite-member-to-organization", authMiddleware, async (req: Request, res: Response) => {
-    const memberUsername = req.body.username
-    const organizationId = req.body.organizationId
-    const userId = req.userId
+organizationRouter.get("/:orgId/members", authMiddleware, adminMiddleware, getMembers)
 
-    const organization = await organizationModel.findOne({
-        _id: organizationId
-    })
-    if (!organization || organization.admin.toString() !== userId) {
-        res.status(411).json({
-            msg: "Either you are not admin OR this org does not exist"
-        })
-        return
-    }
-    const memberUser = await userModel.findOne({
-        username: memberUsername
-    })
-    if (!memberUser) {
-        res.status(404).json({
-            msg: "User not found"
-        })
-        return
-    }
-
-    if (organization.members.includes(memberUser._id)) {
-        res.status(400).json({
-            msg: "User is already in the organization"
-        })
-        return
-    }
-
-    await organizationModel.findByIdAndUpdate(
-        organizationId,
-        {
-            $addToSet: {
-                members: memberUser._id
-            }
-        })
-    res.status(200).json({
-        msg: "User invited succesfully",
-        id: memberUser._id
-    })
-});
-
-
-organizationRouter.get("/organizations", authMiddleware ,async (req: Request, res: Response) => {
-    const userId = req.userId
-    const orgs = await organizationService.getListOfOrgs(userId)
-    if (orgs.length == 0) {
-        res.status(400).json({
-            msg: "You are not part of any organization"
-        })
-        return
-    }
-
-    res.status(200).json({
-        orgs: orgs.map(org => ({
-            id: org._id,
-            title: org.title
-        }))
-    })
-})
-
-organizationRouter.get("/members/:orgId", authMiddleware,adminMiddleware ,async (req: Request, res: Response) => {
-    const orgId = req.params.orgId
-    const members = await organizationService.getMembers(orgId as string)
-
-    if(!members){
-        res.status(411).json({
-            msg : "No members in this organization"
-        })
-        return
-    }
-    res.status(200).json({
-        members: members
-    })
-
-})
-
-organizationRouter.delete("/remove-member/:orgId", authMiddleware, (req: Request, res: Response) => {
-    
-})
+organizationRouter.delete("/:orgId/member/:userId/", authMiddleware, adminMiddleware, removeMember)
