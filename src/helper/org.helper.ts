882bfae26956e9c5a5c9d3c4f4d1bac2b9bbb368
Dhruv Gupta <dhruvgcoder@gmail.com>
2026-05-20 15:58:53 +0530

    helper function removed

--- src/helper/org.helper.ts
+++
@@ -1,8 +0,0 @@
-import { organizationModel } from "../db.js";
-import mongoose from "mongoose";
-
-export const getOrganizations  = async(userId : string | undefined )=>{
-    return await organizationModel.find({
-        members : new mongoose.Types.ObjectId(userId)
-    })
-}
