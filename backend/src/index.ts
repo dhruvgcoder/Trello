9c244a0ef30ff0858db71ee8b96ba6bdaaff1f41
Dhruv Gupta <dhruvgcoder@gmail.com>
2026-06-22 10:26:34 +0530

    Project Restructure

---
+++ backend/src/index.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import { userRouter } from "./routes/user.route.js";
+import { organizationRouter } from "./routes/organization.route.js";
+import { boardsRouter } from "./routes/board.route.js";
+import { issuesRouter } from "./routes/issue.route.js";
+
+
+import { env } from "./env.js";
+
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.use("/users",userRouter)
+app.use("/organization",organizationRouter)
+app.use("/boards",boardsRouter)
+app.use("/issues",issuesRouter)
+
+
+const PORT = process.env.PORT || 3001
+
+mongoose.connect(env.MONGODB_URL)
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server started on port ${PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("Issue in connecting to Database")
+})
+
