3b617a150c02573c7e25c6e758f8789b8382cb88
Dhruv Gupta <dhruvgcoder@gmail.com>
2026-06-22 11:14:04 +0530

    token header updated

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -6,7 +6,7 @@ export function api(path, options = {}, token) {
   };
 
   if (token) {
-    headers.Authorization = `Bearer ${token}`;
+    headers.token = token;
   }
 
   return fetch(`${BASE_URL}${path}`, {
